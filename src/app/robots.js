9b611ab63eef58805d5e2498356583e36ebebd13
Sandeep Kumar Singh <96144530+Sandeep4381@users.noreply.github.com>
2026-07-22 12:14:38 +0530

    Remove host from robot.txt

--- src/app/robots.js
+++ src/app/robots.js
@@ -14,7 +14,7 @@ export default function robots() {
         allow: "/",
       },
     ],
-    sitemap: "https://swarikaro.com/sitemap.xml",
-    host: "https://swarikaro.com",
+    sitemap: "https://swarikaro.com/sitemap.xml"
+    
   };
 }
